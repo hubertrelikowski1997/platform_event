bcb86ceea52b313f9c89c1e933304fcc4cf9f6b5
Hubert <hubert@relikowski.dev>
2024-10-25 18:43:57 +0200

    Add clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,10 +2,5 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      <Image src="/assets/images/logo.svg" width={128} height={32} alt="logo" />
-      <Button>Click me</Button>
-    </div>
-  );
+  return <div>Text</div>;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 import "./globals.css";
 
 const poppins = Poppins({
@@ -23,8 +29,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={poppins.variable}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
